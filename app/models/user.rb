815ee73cd34a602e83ed690186acbf630a23106a
i-to-to-to-mi <individual080@gmail.com>
2020-06-02 23:43:20 +0900

    Active Storage追加
    ■下記インストール
    ・imagemagick
    ・gem 'mini_magick'
    ・Active Storage
    
    ■Userのavatarカラム削除
    ■avatarULの記述をnew.htmlに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :avatar ,presence: true
+
+  has_one_attached :avatar
 end
